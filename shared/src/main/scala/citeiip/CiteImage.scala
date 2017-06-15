cd31174ad52179c5ad5509f6c63d9a84426f4778
Neel Smith <dnsmith.neel@gmail.com>
2017-06-15 08:09:36 -0400

    Framework

---
+++ shared/src/main/scala/citeiip/CiteImage.scala
@@ -0,0 +1,15 @@
+package edu.holycross.shot.citeiip
+
+import edu.holycross.shot.cite._
+
+import scala.util.Try
+
+import scala.scalajs.js
+import js.annotation.JSExport
+
+
+import edu.holycross.shot.cite._
+
+@JSExport case class CiteImage (urn: Cite2Urn) {
+
+}
---
+++ shared/src/test/scala/citeiip/CiteImageSpec.scala
@@ -0,0 +1,16 @@
+package edu.holycross.shot.citeiip
+import org.scalatest.FlatSpec
+import edu.holycross.shot.cite._
+
+
+/**
+*/
+class CiteImageSpec extends FlatSpec {
+
+
+
+  "A citable image" should "do something" in  pending
+
+  
+
+}
