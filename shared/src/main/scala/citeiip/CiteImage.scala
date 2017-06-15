e79d81cc29a20db0805bc88adcef544b12bc84e9
Neel Smith <dnsmith.neel@gmail.com>
2017-06-15 08:27:15 -0400

    Pass first test

--- shared/src/main/scala/citeiip/CiteImage.scala
+++ shared/src/main/scala/citeiip/CiteImage.scala
@@ -10,6 +10,31 @@ import js.annotation.JSExport
 
 import edu.holycross.shot.cite._
 
-@JSExport case class CiteImage (urn: Cite2Urn) {
+
+
+
+@JSExport case class IipSource (baseUrl: String, baseDirectory: String, directoryMap: Map[Cite2Urn,String]) {
+
+
+  def collectionDirectory(urn: Cite2Urn): String = {
+    val collectionUrn = urn.dropExtensions.dropProperty.dropSelector
+    directoryMap(collectionUrn)
+  }
+
+
+  def iip(urn: Cite2Urn): String = {
+    baseUrl + baseDirectory 
+/*
+http://www.homermultitext.org/iipsrv?OBJ=IIP,1.0&FIF=/project/homer/pyramidal/
+
+VenA/
+
+VA012RN_0013.tif
+
+&RGN=0.204,0.2102,0.172,0.0218
+&WID=9000
+&CVT=JPEG
+    */
+  }
 
 }
--- shared/src/test/scala/citeiip/CiteImageSpec.scala
+++ shared/src/test/scala/citeiip/CiteImageSpec.scala
@@ -7,10 +7,21 @@ import edu.holycross.shot.cite._
 */
 class CiteImageSpec extends FlatSpec {
 
+  val baseUrl = "http://www.homermultitext.org/iipsrv?OBJ=IIP,1.0&"
+  val baseDirectory = "/project/homer/pyramidal/"
 
 
-  "A citable image" should "do something" in  pending
+  val vaImgColl = Cite2Urn("urn:cite2:hmt:vaimg.2017a:")
+  val vaDirectory = "VenA"
+  val fileLayout = Map( vaImgColl -> vaDirectory)
 
-  
+  val iipSource = IipSource(baseUrl, baseDirectory, fileLayout)
+
+  "An IIPSource" should "find IIP directories for image URNs" in {
+    val urn = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.204,0.2102,0.172,0.0218")
+
+    val expected = "VenA"
+    assert (iipSource.collectionDirectory(urn) == expected)
+  }
 
 }
